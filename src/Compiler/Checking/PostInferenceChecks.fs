e1875c61c797ddcc731c67507489ca4222182552
ncave <777696+ncave@users.noreply.github.com>
2023-01-31 14:20:17 -0800

    Don't lose attributes of method parameters (#12)
    Temporary fix, remove when upstream dotnet#13786 is fixed.

--- src/Compiler/Checking/PostInferenceChecks.fs
+++ src/Compiler/Checking/PostInferenceChecks.fs
@@ -2391,7 +2391,7 @@ let CheckEntityDefn cenv env (tycon: Entity) =
 
             if numCurriedArgSets > 1 &&
                (minfo.GetParamDatas(cenv.amap, m, minfo.FormalMethodInst)
-                |> List.existsSquared (fun (ParamData(isParamArrayArg, _isInArg, isOutArg, optArgInfo, callerInfo, _, reflArgInfo, ty)) ->
+                |> List.existsSquared (fun (ParamData(isParamArrayArg, _isInArg, isOutArg, optArgInfo, callerInfo, _, reflArgInfo, ty), _) ->
                     isParamArrayArg || isOutArg || reflArgInfo.AutoQuote || optArgInfo.IsOptional || callerInfo <> NoCallerInfo || isByrefLikeTy g m ty)) then
                 errorR(Error(FSComp.SR.chkCurriedMethodsCantHaveOutParams(), m))
 
@@ -2417,7 +2417,7 @@ let CheckEntityDefn cenv env (tycon: Entity) =
                     | ValueNone -> errorR(Error(FSComp.SR.tcCallerInfoWrongType(callerInfo |> string, desiredTyName, NicePrint.minimalStringOfType cenv.denv ty), m))                   
 
                 minfo.GetParamDatas(cenv.amap, m, minfo.FormalMethodInst)
-                |> List.iterSquared (fun (ParamData(_, isInArg, _, optArgInfo, callerInfo, nameOpt, _, ty)) ->
+                |> List.iterSquared (fun (ParamData(_, isInArg, _, optArgInfo, callerInfo, nameOpt, _, ty), _) ->
                     ignore isInArg
 
                     let m =
