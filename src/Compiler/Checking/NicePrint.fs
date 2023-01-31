c2acb047094d4e73caae9ed4ef0235992fc30cfb
ncave <777696+ncave@users.noreply.github.com>
2023-01-31 14:20:17 -0800

    Don't lose attributes of method parameters (#12)
    Temporary fix, remove when upstream dotnet#13786 is fixed.

--- src/Compiler/Checking/NicePrint.fs
+++ src/Compiler/Checking/NicePrint.fs
@@ -1650,7 +1650,7 @@ module InfoMemberPrinting =
             let layout = layoutXmlDocOfMethInfo denv infoReader minfo layout
 
             let paramsL =
-                let paramDatas = minfo.GetParamDatas(amap, m, minst)
+                let paramDatas = minfo.GetParamDatas(amap, m, minst) |> List.map (List.map fst)
                 if List.forall isNil paramDatas then
                     WordL.structUnit
                 else
@@ -1702,6 +1702,7 @@ module InfoMemberPrinting =
                     |> PrintTypes.layoutCsharpCodeAnalysisIlAttributes denv (mi.RawMetadata.Return.CustomAttrs) (squareAngleReturn >> (@@))
                 let paramLayouts = 
                     minfo.GetParamDatas (amap, m, minst)
+                    |> List.map (List.map fst)
                     |> List.head
                     |> List.zip (mi.ParamMetadata)
                     |> List.map(fun (ilParams,paramData) -> 
@@ -1712,6 +1713,7 @@ module InfoMemberPrinting =
             | _ ->
                 layout,
                 minfo.GetParamDatas (amap, m, minst) 
+                |> List.map (List.map fst)
                 |> List.concat 
                 |> List.map (layoutParamData denv)
 
